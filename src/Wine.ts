5d0c81a01b5a9d9d245f8de6a8b3f26ec4855e8a
Brecht Billiet <brecht.billiet@persgroep.be>
2016-02-29 15:21:50 +0100

    fully working rest api

--- src/Wine.ts
+++ src/Wine.ts
@@ -1,12 +1,10 @@
 export class Wine {
-    public id: number;
+    public _id: string;
     constructor(public name: string,
                 public description: string,
                 public region: string,
                 public inStock: number,
                 public price: number,
                 public myRating: number) {
-        this.id = new Date().getTime();
-
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,59 @@
 import * as express from "express";
 import {Express, Request, Response} from "express";
 import {MongoClient} from "mongodb";
-import {Db} from "mongodb";
-import {Wine} from "./Wine";
+import {Db, ObjectID} from "mongodb";
+import * as bodyParser from "body-parser";
 MongoClient.connect(process.env.MONGOLAB_URI, (err: any, db: Db) => {
     console.log("started mongodb with MONGOLAB_URI: " + process.env.MONGOLAB_URI);
+    console.log(err);
     let app: Express = express();
     let port: number = process.env.PORT || 3000;
 
-    console.log(err);
+    app.use(bodyParser.json());
     app.listen(port, () => {
         console.log(`Listening on port ${port}`);
     });
 
     app.get("/wines", (req: Request, res: Response) => {
-        res.send(db.collection("wines").find({}));
+        db.collection("wines").find({}).toArray((error: any, docs: any) => {
+            if(error){
+                res.send(error);
+                return;
+            }
+            res.send(docs);
+        });
+    });
+    app.get("/wines/:id", (req: Request, res: Response) => {
+        db.collection("wines").find({_id: new ObjectID(req.params.id)}).toArray((error: any, docs: any) => {
+            if(error){
+                res.send(error);
+                return;
+            }
+            res.send(docs[0]);
+        });
     });
     app.post("/wines", (req: Request, res: Response) => {
-        db.collection("wines").insertOne(new Wine("test", "test", "test", 0, 0, 4))
+        console.log(req.body);
+        db.collection("wines").insertOne(req.body, (error: any) => {
+            if(error){
+                res.send(error);
+                return;
+            }
+            res.end();
+        })
     });
-    app.post("/wines/:id", (req: Request, res: Response) => {
-
+    app.put("/wines/:id", (req: Request, res: Response) => {
+        db.collection("wines").updateOne({_id: new ObjectID(req.params.id)}, req.body, (error: any) => {
+            if(error){
+                res.send(error);
+                return;
+            }
+            res.end();
+        })
     });
     app.delete("/wines/:id", (req: Request, res: Response) => {
-
+        db.collection("wines").deleteOne({_id: new ObjectID(req.params.id)}, (error: any) => {
+            res.end();
+        });
     });
 });
